9f1389c3fc37a4c36143a43bf3603cead7e1cde2
itochs <chhu20042@g.nihon-u.ac.jp>
2022-04-27 17:48:00 +0900

    lessen2

--- src/pages/Lesson02/index.js
+++ src/pages/Lesson02/index.js
@@ -4,7 +4,8 @@ import Chart from "../../components/Chart02";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  input.sort((a, b) => b.count - a.count)
+  return input.slice(0, 20); // ここを作りましょう！
 };
 
 const Lesson = () => {
